46ac888c9f3888fda35512e322c868bc35bf086a
Jason Wang <jasonprime80704@gmail.com>
2022-12-21 20:49:55 -0500

    counter app MVU

--- architecture.md
+++ architecture.md
@@ -5,6 +5,22 @@
 Models represent state in the app. Models are defined with `type alias`.
 
 ```elm
-type alias Model =
-    Int
+type alias Model = Int
+```
+
+Views represent the UI.
+
+```elm
+view : Model -> Html Msg
+```
+
+Update represents user actions and how the model is updated.
+
+```elm
+type Msg
+  = ClickOnA
+  | ClickOnB
+  | ToggleC
+
+update : Msg -> Model -> Model
 ```
--- src/Counter.elm
+++ src/Counter.elm
@@ -1,8 +1,46 @@
 module Counter exposing (Model)
 
+import Browser
+import Html exposing (Html)
+import Html.Events exposing (..)
+
 type alias Model =
     Int
 
 initialModel : Model
 initialModel =
     0
+
+
+view : Model -> Html Msg
+view model =
+    Html.div []
+        [ Html.button [ onClick Decrement ] [ Html.text "-" ]
+        , Html.text (String.fromInt model)
+        , Html.button [ onClick Increment ] [ Html.text "+" ]
+        ]
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+-- main entry point to the app
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
