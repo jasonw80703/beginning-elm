fc73589f5352a06c1a794920eb9ce90e4e2a9a2f
Jason Wang <jasonprime80704@gmail.com>
2022-12-21 22:58:45 -0500

    signup

---
+++ src/Signup.elm
@@ -0,0 +1,50 @@
+module Signup exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+type alias User =
+    { name : String
+    , email : String
+    , password : String
+    , loggedIn : Bool
+    }
+
+
+initialModel : User
+initialModel =
+    { name = ""
+    , email = ""
+    , password = ""
+    , loggedIn = False
+    }
+
+
+view : User -> Html msg
+view user =
+    div []
+        [ h1 [] [ text "Sign up" ]
+        , Html.form []
+            [ div []
+                [ text "Name"
+                , input [ id "name", type_ "text" ] []
+                ]
+            , div []
+                [ text "Email"
+                , input [ id "email", type_ "email" ] []
+                ]
+            , div []
+                [ text "Password"
+                , input [ id "password", type_ "password" ] []
+                ]
+            , div []
+                [ button [ type_ "submit" ]
+                    [ text "Create my account" ]
+                ]
+            ]
+        ]
+
+
+main : Html msg
+main =
+    view initialModel
